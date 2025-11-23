4c4f4594a48a99d2958884908baa692d62589794
penhin <2977923221@qq.com>
2025-11-23 20:45:03 +0800

    finish hw9

--- src/main/java/com/itheima/reggie/service/CategoryService.java
+++ src/main/java/com/itheima/reggie/service/CategoryService.java
@@ -2,6 +2,8 @@ package com.itheima.reggie.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
   import com.itheima.reggie.entity.Category;
-  
-  public interface CategoryService extends IService<Category> {
-  }
+
+public interface CategoryService extends IService<Category> {
+    //根据ID删除分类
+    public void remove(Long id);
+}
