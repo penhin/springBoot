394cb84e62f7a4681b598bdc49cc931cee995f1e
penhin <2977923221@qq.com>
2025-12-01 09:43:48 +0800

    finish 9.1-9.2

--- src/main/java/com/itheima/reggie/controller/DishController.java
+++ src/main/java/com/itheima/reggie/controller/DishController.java
@@ -1,5 +1,10 @@
 package com.itheima.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.reggie.Dto.DishDto;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.Dish;
 import com.itheima.reggie.service.DishFlavorService;
   import com.itheima.reggie.service.DishService;
   import lombok.extern.slf4j.Slf4j;
@@ -17,4 +22,33 @@ import com.itheima.reggie.service.DishFlavorService;
   
       @Autowired
       private DishFlavorService dishFlavorService;
+
+      /**
+       * 分页查询
+       * @param page
+       * @param pageSize
+       * @return
+       */
+      @GetMapping("/page")
+      public R<Page> page(int page, int pageSize){
+          //分页构造器
+          Page<Dish> pageInfo = new Page<>(page,pageSize);
+          //条件构造器
+          LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+          //添加排序条件，根据sort进行排序
+          queryWrapper.orderByAsc(Dish::getSort);
+
+          //分页查询
+          dishService.page(pageInfo,queryWrapper);
+          return R.success(pageInfo);
+      }
+
+      @PostMapping
+      public R<String> save(@RequestBody DishDto dishDto) {
+          log.info(dishDto.toString());
+
+          dishService.saveWithFlavor(dishDto);
+
+          return R.success("保存菜品成功");
+      }
   }    
--- src/main/java/com/itheima/reggie/service/impl/DishServiceImpl.java
+++ src/main/java/com/itheima/reggie/service/impl/DishServiceImpl.java
@@ -1,13 +1,38 @@
 package com.itheima.reggie.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-  import com.itheima.reggie.entity.Dish;
-  import com.itheima.reggie.mapper.DishMapper;
-  import com.itheima.reggie.service.DishService;
-  import lombok.extern.slf4j.Slf4j;
-  import org.springframework.stereotype.Service;
-  
-  @Service
-  @Slf4j
-  public class DishServiceImpl extends ServiceImpl<DishMapper,Dish> implements DishService {
-  }
+import com.itheima.reggie.Dto.DishDto;
+import com.itheima.reggie.entity.Dish;
+import com.itheima.reggie.entity.DishFlavor;
+import com.itheima.reggie.mapper.DishMapper;
+import com.itheima.reggie.service.DishFlavorService;
+import com.itheima.reggie.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Transactional
+@Service
+@Slf4j
+public class DishServiceImpl extends ServiceImpl<DishMapper,Dish> implements DishService {
+    @Autowired
+    private DishFlavorService dishFlavorService;
+
+    @Override
+    public void saveWithFlavor(DishDto dishDto) {
+        this.save(dishDto);
+        Long dishID = dishDto.getId();
+
+        List<DishFlavor> flavors = dishDto.getFlavors();
+        List<DishFlavor> flavorList = flavors.stream().map(item -> {
+            item.setDishId(dishID);
+            return item;
+        }).collect(Collectors.toList());
+
+        dishFlavorService.saveBatch(flavorList);
+    }
+}
